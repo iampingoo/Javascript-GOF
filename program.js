3e6fdc6bb43db6d921aaa518b3bf8b6d591e63f1
iampingoo <iampingoo@yahoo.co.jp>
2018-03-27 23:13:52 +0900

    Add files via upload

---
+++ program.js
@@ -0,0 +1,35 @@
+// DOMへの参照
+const    controlCheckbox    = document.getElementById( "mainCheckbox" )
+        ,addBtn             = document.getElementById( "addNewObserver")
+        ,container          = document.getElementById( "observersContainer" );
+
+// 具象サブジェクト
+
+// 制御用チェックボックスをサブジェクトクラスで拡張
+extend( new Subject(), controlCheckbox );
+
+// チェックボックスをクリックするとオブザーバに通知する
+controlCheckbox.onclick = () => { controlCheckbox.Notify( controlCheckbox.checked ) };
+
+addBtn.onclick          = AddNewObserver;
+
+// 具象オブザーバ
+function AddNewObserver() {
+    // 追加するチェックボックスを新たに作成
+    const check = document.createElement( "input" );
+    check.type  = "checkbox";
+
+    // オブザーバクラスでチェックボックスを拡張
+    extend( new Observer(), check );
+
+    // カスタム更新動作でオーバーライド
+    check.Update    = ( value ) => {
+        check.checked    = value;
+    };
+
+    // メインサブジェクトのオブザーバのリストに新しいオブザーバを追加する
+    controlCheckbox.AddObserver( check );
+
+    // コンテナに項目を追加
+    container.appendChild( check );
+}
